bab3e73be1d16f29f5c14b3097e64ab6bf192170
Mylika <vikamakovnikova975@gmail.com>
2021-01-10 23:14:28 +1000

    спрайты готовы, но кратинки не обрезаются почему-то. Может картинки не правильные

--- test (3).py
+++ test (3).py
@@ -1,4 +1,40 @@
 import pygame
+import os
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+size = width, height = screen.get_size()
+screen.fill('#C0F400')
+
+
+def load_image(name, colorkey=-9):
+    fullname = os.path.join('picture', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    image1 = pygame.transform.scale(image, (100, 100))
+    return image1
+
+
+all_sprites = pygame.sprite.Group()
+sprite_p = pygame.sprite.Sprite()
+sprite_p.image = load_image("plant1.jpg")
+sprite_p.rect = sprite_p.image.get_rect()
+sprite_s = pygame.sprite.Sprite()
+sprite_s.image = load_image("trava.png")
+sprite_s.rect = sprite_s.image.get_rect()
+all_sprites.add(sprite_s)
+all_sprites.add(sprite_p)
 
 
 class Board:
@@ -6,7 +42,6 @@ class Board:
         self.width = width
         self.height = height
         self.board = [[0] * width for _ in range(height)]
-
         self.left = int(w * 0.15)
         self.top = int(h * 0.15)
         self.cell_size = int(h * 0.11)
@@ -21,7 +56,7 @@ class Board:
         for i in range(self.height):
             for j in range(self.width):
                 pygame.draw.rect(screen, '#573B08', (x, y, self.cell_size, self.cell_size), 1)
-                if self.board[i][j] ==1:
+                if self.board[i][j] == 1:
                     pygame.draw.rect(screen, '#644F28', (x + 1, y + 1, self.cell_size - 2, self.cell_size - 2))
                 else:
                     pygame.draw.rect(screen, '#865F1A', (x + 1, y + 1, self.cell_size - 2, self.cell_size - 2))
@@ -48,6 +83,8 @@ class Board:
         cell = self.get_cell(mouse_pos)
         if cell:
             self.on_click(cell)
+        sprite_s.rect.x = mouse_pos[0]
+        sprite_s.rect.y = mouse_pos[1]
 
     def on_click(self, cell):
         self.board[cell[1]][cell[0]] = 1
@@ -60,16 +97,16 @@ class Bottom:
 
     def draw(self):
         color = pygame.Color('#FFAA00')
-        pygame.draw.rect(screen, color, (self.w * 0.04, self.h * 0.83, self.w * 0.3, self.h * 0.12))
-        pygame.draw.rect(screen, color, (self.w * 0.4, self.h * 0.83, self.w * 0.13, self.h * 0.12))
-        pygame.draw.rect(screen, color, (self.w * 0.78, self.h * 0.83, self.w * 0.17, self.h * 0.12))
+        pygame.draw.rect(screen, color,
+                         (int(self.w * 0.1), int(self.h * 0.85), int(self.w * 0.3),
+                          int(self.h * 0.15)))  # панель выбора растения
+        pygame.draw.rect(screen, color,
+                         (int(self.w * 0.45), int(self.h * 0.85), int(self.w * 0.1),
+                          int(self.h * 0.15)))  # мотыга, грабля
+        pygame.draw.rect(screen, color,
+                         (int(self.w * 0.79), int(self.h * 0.85), int(self.w * 0.2), int(self.h * 0.15)))  # меню
         pygame.draw.rect(screen, color, (self.w * 0.6, 0, self.w * 0.3, self.h * 0.12))
 
-
-pygame.init()
-size = width, height = 1150, 700
-screen = pygame.display.set_mode(size)
-screen.fill('#C0F400')
 
 board = Board(8, 6, width, height)
 Bottom(width, height).draw()
@@ -81,8 +118,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             board.get_click(event.pos)
+            all_sprites.draw(screen)
 
     board.render()
     pygame.display.flip()
